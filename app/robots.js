1c23e85ccbd7565c6ec91c72ce85c683cfac3395
thyking7TK <mrewa.t@northeastern.edu>
2026-04-15 15:07:17 -0400

    Fix domain to autoviseblackcar.com

--- app/robots.js
+++ app/robots.js
@@ -7,6 +7,6 @@ export default function robots() {
         disallow: ["/admin", "/api/"],
       },
     ],
-    sitemap: "https://autoblackcar.com/sitemap.xml",
+    sitemap: "https://autoviseblackcar.com/sitemap.xml",
   };
 }
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://autoblackcar.com",
+      url: "https://autoviseblackcar.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
